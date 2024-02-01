29943a6aa229e4989e096490102f3fe88956f733
Rachelle <rrosiles@hawk.iit.edu>
2024-01-31 18:27:44 -0600

    Initial Commit

---
+++ 2.2_subcansum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2.2_subcansum
+//
+//  Created by PHYS 440 Rachelle on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2.2_subcansum/__2_subcansumApp.swift
@@ -0,0 +1,17 @@
+//
+//  __2_subcansumApp.swift
+//  2.2_subcansum
+//
+//  Created by PHYS 440 Rachelle on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct __2_subcansumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
